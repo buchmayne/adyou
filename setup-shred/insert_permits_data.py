8c540f21ab0eb9670270da3f21459c76d20b9743
buchmayne <michaelmbuchman@gmail.com>
2020-08-03 19:49:22 -0700

    added program to add permits data to postgis

---
+++ setup-shred/insert_permits_data.py
@@ -0,0 +1,10 @@
+from sqlalchemy import create_engine
+import geopandas as gpd
+
+db_connection_url = "postgres://postgres:password@localhost:5432/shred"
+engine = create_engine(db_connection_url)
+
+path_to_permits_data = '../data/Residential_Building_Permits.shp'
+permits = gpd.read_file(path_to_permits_data)
+
+permits.to_postgis('permits', con=engine, if_exists='replace', index=True)
